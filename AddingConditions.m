02e75c4193c567b3c60787e97c0b490be9b76205
DulceMariscal <mariscaldulce@gmail.com>
2021-05-04 09:31:11 -0400

    Update AddingConditions.m

--- AddingConditions.m
+++ AddingConditions.m
@@ -1,5 +1,5 @@
 %Chagingparams 
-function adaptData= AddingConditions(in,NewConditions,indexNewCond)
+function adaptData= AddingConditions(in,NewConditions,indexNewCond,description)
 
 idxfast=find(compareListsNested({'singleStanceSpeedFastAbsANK'},in.data.labels)==1);
 idxslow=find(compareListsNested({'singleStanceSpeedSlowAbsANK'},in.data.labels)==1);
@@ -16,11 +16,15 @@ idxSplit=find(difference>200);
 for i=1:length(NewConditions)
     in.metaData.conditionName{i}=NewConditions{i};
     in.metaData.trialsInCondition{i}=i;
+    in.metaData.conditionDescription{i}= description{i};
+    in.data.trialTypes{i}='TM';
+
 %     adaptData.metaData.conditionName{3}='Adaptation';
 %     adaptData.metaData.conditionName{4}='Washout';
 end
 in.data.Data(idxSplit(1):idxSplit(end),4)=indexNewCond(1);
 in.data.Data(idxSplit(end)+1:end,4)=indexNewCond(2);
+
 
 %%
 params={'stepLengthAsym'};
@@ -28,5 +32,5 @@ in.plotAvgTimeCourse(in,params)
 
 adaptData=in;
 
-save([adaptData.subData.ID 'paramsNewConditions.mat'],'adaptData','-v7.3');
+save([adaptData.subData.ID 'paramsNewConditionsV2.mat'],'adaptData','-v7.3');
 end 
