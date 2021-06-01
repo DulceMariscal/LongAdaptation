1f700fdfa35322c97d87b7cb342f2c3d5ef9cdf1
Alyssa-Long <arl130@pitt.edu>
2021-06-01 14:07:12 -0400

    Update Csv2mat.m

--- Sync_Scripts/Csv2mat.m
+++ Sync_Scripts/Csv2mat.m
@@ -1,8 +1,8 @@
 %cvs2mat EMG data 
 
-dir1='Y:\Dulce\R01_Nimbus2021\VROG_02\PC1';
-dir2='Y:\Dulce\R01_Nimbus2021\VROG_02\PC2';
-trials= [21];
+dir1='Y:\Dulce\R01_Nimbus2021\EMGTestFreq - Copy\PC1';
+dir2='Y:\Dulce\R01_Nimbus2021\EMGTestFreq - Copy\PC2';
+trials= [1,3:10,13];
 
 Getting_EMGdata_Csv2mat_PC1(dir1, trials)
 Getting_EMGdata_Csv2mat_PC2(dir2, trials)
