6ee6e7b15959237b3d0248f5ed89559ef27dd689
dum5 <37150306+DulceMariscal@users.noreply.github.com>
2021-12-08 09:46:22 -0500

    Csv2mat update to remove the PC names

--- Sync_Scripts/Csv2mat.m
+++ Sync_Scripts/Csv2mat.m
@@ -1,10 +1,10 @@
 %cvs2mat EMG data 
 
-dir1='Y:\Dulce\DataBase2\Pilot2\PC1';
-dir2='Y:\Dulce\DataBase2\Pilot2\PC2';
-% trials= [1:5 9:15 17:19];
-trials=[2:5 9];
-% Getting_EMGdata_Csv2mat_PC1(dir1, trials)
-Getting_EMGdata_Csv2mat_PC1(dir2, trials)
+dir1='S:\Shared\Dulce\EMG_Generalization\ATS03\PC1';
+dir2='S:\Shared\Dulce\EMG_Generalization\ATS03\PC2';
+trials= [3:16];
+% trials=[19];
+Getting_EMGdata_Csv2mat(dir1, trials)
+Getting_EMGdata_Csv2mat(dir2, trials)
 
 
