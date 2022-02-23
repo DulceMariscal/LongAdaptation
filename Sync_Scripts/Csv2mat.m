084f41c560caac6da87b94d73f50552bcf8edfb9
MarcelaGonzalezRubio <33523484+MarcelaGonzalezRubio@users.noreply.github.com>
2022-02-23 11:48:41 -0500

    Sync Data to work with one PC
    - I added a bunch of if whenever PC2 was used in the code to check if the directory was empty or not.
    - Added a loop to automatically generate the tts vector.
    - Some Figures were commented out (so comment them back in if needed)

--- Sync_Scripts/Csv2mat.m
+++ Sync_Scripts/Csv2mat.m
@@ -1,10 +1,10 @@
-%cvs2mat EMG data 
+%cvs2mat EMG data (Create the EMG mat files from the csv files)
 
-dir1='S:\Shared\Dulce\EMG_Generalization\ATS03\PC1';
-dir2='S:\Shared\Dulce\EMG_Generalization\ATS03\PC2';
-trials= [3:16];
+dir1='Y:\Marcela\Perception Studies\Weber Perception\Data\WP01\Session 1\EMG';
+% dir2='S:\Shared\Dulce\EMG_Generalization\ATS03\PC2';
+trials= [6:12];
 % trials=[19];
 Getting_EMGdata_Csv2mat(dir1, trials)
-Getting_EMGdata_Csv2mat(dir2, trials)
+% Getting_EMGdata_Csv2mat(dir2, trials)
 
 
