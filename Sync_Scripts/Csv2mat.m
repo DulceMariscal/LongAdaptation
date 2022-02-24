d534c213b60e3f9bfa4bb5949032c1f217e9b333
MarcelaGonzalezRubio <33523484+MarcelaGonzalezRubio@users.noreply.github.com>
2022-02-24 10:19:58 -0500

    Make it a function
    Everything will now run from csv2mat script

--- Sync_Scripts/Csv2mat.m
+++ Sync_Scripts/Csv2mat.m
@@ -1,10 +1,28 @@
 %cvs2mat EMG data (Create the EMG mat files from the csv files)
 
+clc
+close all
+clear all
+
+
+dirNexus='Y:\Marcela\Perception Studies\Weber Perception\Data\WP01\Session 1';
 dir1='Y:\Marcela\Perception Studies\Weber Perception\Data\WP01\Session 1\EMG';
-% dir2='S:\Shared\Dulce\EMG_Generalization\ATS03\PC2';
-trials= [6:12];
-% trials=[19];
-Getting_EMGdata_Csv2mat(dir1, trials)
-% Getting_EMGdata_Csv2mat(dir2, trials)
+
+% Leave empty if you only use one computer
+dir2=[];
+
+trialNumber= [6:12]; % List of the trial numbers from the session
+
+% Generate the empty mat files from the
+Getting_EMGdata_Csv2mat(dir1, trialNumber)
+
+if ~isempty(dir2)
+    Getting_EMGdata_Csv2mat(dir2, trialNumber)    
+end
+
+
+% Syncronize EMG signals 
+
+Sync_EMG_Signal_allData(dirNexus, dir1, dir2, trialNumber);
 
 
